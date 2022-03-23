a705896a81def4f4bb20e9894f7afde28113dd76
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-03-23 22:45:13 +0300

    main

--- JS23-mar-2022/Task4/Task4.js
+++ JS23-mar-2022/Task4/Task4.js
@@ -1,23 +1,25 @@
 function fontFam() {
     let x = document.getElementById("F").value;
     if (x === 'Arial') {
-        document.getElementById("TB").style.fontFamily = x;  }
-     if (x === 'TimesNewRoman') {
         document.getElementById("TB").style.fontFamily = x;
     }
-     if (x === 'fantasy') {
+    if (x === 'TimesNewRoman') {
         document.getElementById("TB").style.fontFamily = x;
     }
-     if (x === 'cursive') {
+    if (x === 'fantasy') {
+        document.getElementById("TB").style.fontFamily = x;
+    }
+    if (x === 'cursive') {
         document.getElementById("TB").style.fontFamily = x;
     }
 }
 
 
 function fontSize() {
+
     let x = document.getElementById("s").value;
     if (x === '12') {
-        document.getElementById("TB").style.fontSize=x+"px";
+        document.getElementById("TB").style.fontSize = x + "px";
     }
     if (x === '18') {
         document.getElementById("TB").style.fontSize = "18px";
@@ -33,16 +35,37 @@ function fontSize() {
 
 
 function fontItalic() {
+    italic = false;    
     let x = document.getElementById("a").value;
-    document.getElementById("TB").style.fontStyle = x;
+    if (document.getElementById("a").checked == true) {
+        italic = true;
+
+        document.getElementById("TB").style.fontStyle = x;
     }
+    else {
+        document.getElementById("TB").style.fontStyle = "normal";
+    }
+    
+}
 
 function fontBold() {
+
     let x = document.getElementById("b").value;
-    document.getElementById("TB").style.fontWeight = x;
+    if (document.getElementById("b").checked == true) {
+        document.getElementById("TB").style.fontWeight = x;
+    }
+    else {
+        document.getElementById("TB").style.fontWeight  = "normal";
+    }
 }
 
 function fontUderLine() {
     let x = document.getElementById("c").value;
-    document.getElementById("TB").style.textDecoration = x;
+    if (document.getElementById("c").checked == true) {
+        document.getElementById("TB").style.textDecoration = x;
+    }
+    else {
+        document.getElementById("TB").style.textDecoration="none";
+    }
+
 }
