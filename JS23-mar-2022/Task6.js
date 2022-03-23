77aef462797ae086c3114c0e5d3a05e599c8bef0
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-03-23 17:02:41 +0300

    main

---
+++ JS23-mar-2022/Task6.js
@@ -0,0 +1,25 @@
+function addSong() { 
+    let add = document.createElement("li");
+    let inputValue = document.getElementById('write').value;
+    var t = document.createTextNode(inputValue);
+    add.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write a song name!");
+    } else {
+        document.getElementById("song").appendChild(add);
+    }
+
+    document.getElementById("write").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+}
+
+function bcolor() {
+    document.getElementById("write").className = document.getElementById("write")+"write";
+
+}
