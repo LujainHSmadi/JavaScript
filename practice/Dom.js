7ca76c1e4ef107b0e768194d8926b3eae757ea90
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-03-29 11:00:52 +0300

    main

--- practice/Dom.js
+++ practice/Dom.js
@@ -6,12 +6,3 @@ newDiv.setAttribute('title', 'Hello Div');
 var newDivText = document.createTextNode("Hello Iam Lujain and this div is created on jS");
 newDiv.appendChild(newDivText);
 console.log(newDiv);
-var xx = 5;
-let y = 9;
-
-function x() {
-    var xx = 5;
-    y = y+1;
- }
-
- console.log(y);
