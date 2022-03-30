e5ba901f14489b0bc4c9ca1c6132b21dc2884671
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-03-30 20:07:56 +0300

    main

---
+++ JS30-mar-2022/regex.js
@@ -0,0 +1,18 @@
+let text = "Hello all every All ?"
+let pattern = /All/;
+
+let result = text.match(pattern);// retreave the patteren if it matches 
+
+document.getElementById("pat").innerHTML = result;
+
+/**/
+
+let myString = "Hello Elzero Web School I Love elzero";
+
+let regex = /Elzero/;
+
+console.log(myString.match(regex));
+
+
+
+
